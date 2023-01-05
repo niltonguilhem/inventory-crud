625a3122ab20584ac0ec77b52c9148f135d2d6d4
niltonguiilhem <nil.guilhem@gmail.com>
2023-01-05 16:07:43 -0300

    Swagger

--- src/main/java/com/example/inventory/InventoryApplication.java
+++ src/main/java/com/example/inventory/InventoryApplication.java
@@ -4,11 +4,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import javax.management.remote.JMXServerErrorException;
 import javax.naming.NamingException;
 
 @SpringBootApplication
+@EnableWebMvc
 public class InventoryApplication {
 
 	private static Logger logger = LoggerFactory.getLogger(InventoryApplication.class);
